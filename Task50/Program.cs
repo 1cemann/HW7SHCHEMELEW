dace4d01d23c3d7b8beb8500afef929171008b0c
Mad <ned_91@mail.ru>
2022-06-29 01:52:08 +0300

    промежуточная версия

---
+++ Task50/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 50: Напишите программу, которая на вход принимает позиции элемента в 
+//двумерном массиве, и возвращает значение этого элемента или же указание, 
+//что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+using System;
+using static System.Console;
+Clear();
+Write("Введите число:  ");
+int find=int.Parse(ReadLine());
+
+
+int[,] matrix = new int[4, 4];
+GetRandomArray(matrix);
+PrintArray(matrix);
+
+
+
+int GetNumberInArray(int[,] matr)
+{
+    if(position[0]<=m&&position[1]<=n&&position[0]>=0&&position[1]>=0)
+    {
+        int result = array[position[0]-1, position[1]-1];
+        Console.Write($"Значение элемента: {result}");
+    }
+    else Console.Write($"такого элемента в массиве нет."); 
+}
+
+
+
+
+
+
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Write($"{matr[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+void GetRandomArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 100); 
+        }
+    }
+}
